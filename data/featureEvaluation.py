53b3a7b265737d9ca68ee0b88e2b2a1906a8e678
apangasa <60531810+apangasa@users.noreply.github.com>
2021-11-07 12:02:52 -0500

    Update featureEvaluation.py
    Edit filepaths

--- data/featureEvaluation.py
+++ data/featureEvaluation.py
@@ -2,6 +2,8 @@ import json
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 
 def assess_features(X, y, feature_names):
@@ -28,9 +30,9 @@ def assess_features(X, y, feature_names):
 
     print('Feature Importance based on Decision Tree')
     for idx, val in enumerate(feature_importances):
-        print(FEATURE_NAMES[idx], val)
+        print(feature_names[idx], val)
     plt.bar(list(range(len(feature_importances))), feature_importances)
-    plt.xticks(list(range(len(feature_importances))), FEATURE_NAMES)
+    plt.xticks(list(range(len(feature_importances))), feature_names)
     plt.title('Feature Importance based on Decision Tree')
     plt.show()
     print()
@@ -41,23 +43,25 @@ def assess_features(X, y, feature_names):
 
     print('Feature Importance based on Random Forest')
     for idx, val in enumerate(feature_importances):
-        print(FEATURE_NAMES[idx], val)
+        print(feature_names[idx], val)
     plt.bar(list(range(len(feature_importances))), feature_importances)
-    plt.xticks(list(range(len(feature_importances))), FEATURE_NAMES)
+    plt.xticks(list(range(len(feature_importances))), feature_names)
     plt.title('Feature Importance based on Random Forest')
     plt.show()
 
 
 if __name__ == '__main__':
 
-    X, y = None, None
+    X, y, feature_names = None, None, None
 
-    with open('X.json', 'r') as X_file:
+    with open('features-and-labels/X.json', 'r') as X_file:
         X = json.load(X_file)
-    with open('y.json', 'r') as y_file:
+    with open('features-and-labels/y.json', 'r') as y_file:
         y = json.load(y_file)
+    with open('features-and-labels/feature_names.json', 'r') as feature_name_file:
+        feature_names = json.load(feature_name_file)
 
-    feature_names = ['Duration', 'Mean Amplitude', 'Mean RMSE', 'ZCR', 'Mean Spectral Centroid', 'Mean Spectral Rolloff RP=0.1', 'Mean Spectral Rolloff RP=0.2', 'Mean Spectral Rolloff RP=0.3', 'Mean Spectral Rolloff RP=0.4', 'Mean Spectral Rolloff RP=0.5', 'Mean Spectral Rolloff RP=0.6', 'Mean Spectral Rolloff RP=0.7', 'Mean Spectral Rolloff RP=0.8', 'Mean Spectral Rolloff RP=0.9', 'Mean MFCC 1', 'Mean MFCC 2', 'Mean MFCC 3', 'Mean MFCC 4',
-                     'Mean MFCC 5', 'Mean MFCC 6', 'Mean MFCC 7', 'Mean MFCC 8', 'Mean MFCC 9', 'Mean MFCC 10', 'Mean MFCC 11', 'Mean MFCC 12', 'Mean MFCC 13', 'Mean MFCC 14', 'Mean MFCC 15', 'Mean MFCC 16', 'Mean MFCC 17', 'Mean MFCC 18', 'Mean MFCC 19', 'Mean MFCC 20', 'Mean Spectral Contrast 1', 'Mean Spectral Contrast 2', 'Mean Spectral Contrast 3', 'Mean Spectral Contrast 4', 'Mean Spectral Contrast 5', 'Mean Spectral Contrast 6', 'Mean Spectral Contrast 7']
+    # feature_names = ['Duration', 'Mean Amplitude', 'Mean RMSE', 'ZCR', 'Mean Spectral Centroid', 'Mean Spectral Rolloff RP=0.1', 'Mean Spectral Rolloff RP=0.2', 'Mean Spectral Rolloff RP=0.3', 'Mean Spectral Rolloff RP=0.4', 'Mean Spectral Rolloff RP=0.5', 'Mean Spectral Rolloff RP=0.6', 'Mean Spectral Rolloff RP=0.7', 'Mean Spectral Rolloff RP=0.8', 'Mean Spectral Rolloff RP=0.9', 'Mean MFCC 1', 'Mean MFCC 2', 'Mean MFCC 3', 'Mean MFCC 4',
+    #                  'Mean MFCC 5', 'Mean MFCC 6', 'Mean MFCC 7', 'Mean MFCC 8', 'Mean MFCC 9', 'Mean MFCC 10', 'Mean MFCC 11', 'Mean MFCC 12', 'Mean MFCC 13', 'Mean MFCC 14', 'Mean MFCC 15', 'Mean MFCC 16', 'Mean MFCC 17', 'Mean MFCC 18', 'Mean MFCC 19', 'Mean MFCC 20', 'Mean Spectral Contrast 1', 'Mean Spectral Contrast 2', 'Mean Spectral Contrast 3', 'Mean Spectral Contrast 4', 'Mean Spectral Contrast 5', 'Mean Spectral Contrast 6', 'Mean Spectral Contrast 7']
 
     assess_features(X, y, feature_names)
