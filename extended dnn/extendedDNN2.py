6155dd9c65477e4d02b23451e31df2d6e191aba4
apangasa <60531810+apangasa@users.noreply.github.com>
2021-12-16 19:26:51 -0500

    Add evaluation of test dataset

--- extended dnn/extendedDNN2.py
+++ extended dnn/extendedDNN2.py
@@ -18,13 +18,15 @@ tf.get_logger().setLevel('ERROR')
 
 X = pd.read_json('./data/features-and-labels/X.json')
 Y = pd.read_json('./data/features-and-labels/y.json')
+X_test = pd.read_json('./data/features-and-labels/X_test.json')
+Y_test = pd.read_json('./data/features-and-labels/y_test.json')
 
 # X = X.iloc[:, 32:]
 # features selected based on importance > 0.02
 X = X[[0, 2, 5, 14, 16, 19, 20, 23, 28, 34, 39, 40]]
 # > 0.025
 X = X[[0, 2, 14, 23, 28, 34, 40]]
-
+X_test = X_test[[0, 2, 14, 23, 28, 34, 40]]
 
 tf.random.set_seed(0)
 
@@ -33,9 +35,11 @@ X_train, X_val, Y_train, Y_val = train_test_split(
 
 Y_train = to_categorical(Y_train)
 Y_val = to_categorical(Y_val)
+Y_test = to_categorical(Y_test)
+
 X_train = X_train.values
 X_val = X_val.values
-
+X_test = X_test.values
 
 # DNN
 modelDNN = keras.Sequential([
@@ -71,6 +75,7 @@ history = modelDNN.fit(X_train, Y_train,
 
 modelDNN.evaluate(X_train, Y_train)
 modelDNN.evaluate(X_val, Y_val)
+modelDNN.evaluate(X_test, Y_test)
 
 pred_train = modelDNN.predict_classes(X_train)
 pred_val = modelDNN.predict_classes(X_val)
